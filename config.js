373b32f0f24d6fdae7c9a8ea6b7827de5fb89692
Viscerall <nizaradisaputra12@gmail.com>
2026-07-31 03:18:02 +0700

    detail add 3

--- config.js
+++ config.js
@@ -90,8 +90,17 @@ const CONFIG = {
 
     // 6. Galeri Foto
     galeri: [
-        { url: "assets/galeri-1.jpg", kategori: "Liburan" },
-        { url: "assets/galeri-2.jpg", kategori: "Video Call" },
-        { url: "assets/galeri-3.jpg", kategori: "Random" }
+        { url: "assets/galeri-1.jpg", kategori: "1" },
+        { url: "assets/galeri-2.jpg", kategori: "2" },
+        { url: "assets/galeri-3.jpg", kategori: "3" },
+        { url: "assets/galeri-4.jpg", kategori: "1" },
+        { url: "assets/galeri-5.jpg", kategori: "2" },
+        { url: "assets/galeri-6.jpg", kategori: "3" },
+        { url: "assets/galeri-7.jpg", kategori: "1" },
+        { url: "assets/galeri-8.jpg", kategori: "2" },
+        { url: "assets/galeri-9.jpg", kategori: "3" },
+        { url: "assets/galeri-10.jpg", kategori: "1" },
+        { url: "assets/galeri-11.jpg", kategori: "2" },
+        { url: "assets/galeri-12.jpg", kategori: "3" },
     ]
 };
