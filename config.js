b7f53c35be157bd4bb7d1a89a38d0983432d7ce4
Viscerall <nizaradisaputra12@gmail.com>
2026-07-31 01:59:08 +0700

    detail add 1

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ const CONFIG = {
     // 1. Data Pasangan
     pasangan: {
         pihak1: {
-            nama: "Nama Kamu",
-            kota: "Bogor, Indonesia",
+            nama: "Nizuy",
+            kota: "Bogor",
             foto: "assets/foto-saya.jpg"
         },
         pihak2: {
-            nama: "Nama Pasangan",
-            kota: "Kota Pasangan",
-            foto: "assets/foto-pasangan.jpg"
+            nama: "Vanyuy",
+            kota: "Sumedang",
+            foto: "assets/foto-pasangan.jpeg"
         }
     },
 
@@ -18,23 +18,59 @@ const CONFIG = {
 
     // 3. Target Pertemuan Berikutnyagit 
     nextMeeting: {
-        tanggal: "2027-01-01T00:00:00",
-        lokasi: "Stasiun/Bandara/Kafe Favorit"
+        tanggal: "2026-08-16T10:00:00",
+        lokasi: "Stasiun Bogor"
     },
 
     // 4. Timeline Momen Penting
     timeline: [
         {
-            tanggal: "15 Agustus 2023",
-            judul: "Pertama Kali Bertemu",
-            cerita: "Tulis cerita singkat tentang bagaimana kalian pertama kali bertemu di sini.",
+            tanggal: "25 Oktober 2025",
+            judul: "Our First Meet",
+            cerita: "Ini pertama kali banget kita ketemu waktu on boarding probies, inget banget momen ini rada hoki soalnya pas awal liat kamu kaya WOW cakep juga, eh terus pas foto bareng tim FnL sampingan mwehehehe mana pose rangkulan, STRIKEEE aku orang ter hoki sedunia hari itu",
             foto: "assets/timeline-1.jpg"
         },
         {
-            tanggal: "1 Januari 2024",
-            judul: "Mulai LDR",
-            cerita: "Tulis pesan semangat tentang keputusan kalian memulai LDR.",
-            foto: "assets/timeline-2.jpg"
+            tanggal: "1 Maret 2026",
+            judul: "What If Moment",
+            cerita: "What if.. aku ga ucapin kamu birthday dan kamu ga narik aku buat reterm, disini lucu banget dah chattan kita, kaya orang make woila. ISTG aku usaha mati matian biar chat kita ga mati soalnya disini udah di fase crushin nyonyoy hehehe",
+            foto: "assets/timeline-2.PNG"
+        },
+        {
+            tanggal: "18 April 2026",
+            judul: "First Date(?)",
+            cerita: "Ini pertama kali banget kita main, waktu itu akal akalan mau nganterin kamu balik ke stasiun ehh geser dulu ke Snday (tragedi hows life ada di moment ini kalo kamu lupa, iya kamu gubrak pas baru banget duduk hahahaha), abistu kita muter muter cari charger dulu dan akhirnya kita berdua nunggu sambil ngobrol ngobrol, first time nyender ke noynoy yeaayyy",
+            foto: "assets/timeline-3.jpg"
+        },
+        {
+            tanggal: "18 Mei 2026",
+            judul: "Date ke-1252718 kali nya",
+            cerita: "Ini kita lagi di Zama House, pertama kalinya aku bikin nyonyoy minum kopi wlekkk, kamu kalah taruhan gagal bikin aku nonton horor sebelum aku bikin kamu minum kopi",
+            foto: "assets/timeline-4.jpg"
+        },
+        {
+            tanggal: "1 Juni 2026",
+            judul: "Our First Photobooth",
+            cerita: "Ceilahh udah kaya orang bener ni segala photobooth, untung waktu itu kita ke acara kalisuy jadi ada moment ini dan bisa liat jendral kita naik stager wkwkwkwk",
+            foto: "assets/timeline-5.jpeg"
+        },
+        {
+            tanggal: "5 Juli 2026",
+            judul: "Last Date Before KKN",
+            cerita: "Ini moment paling campur aduk huhuhu, rasanya gamau ada hari besok. Sedih mau ditinggal jauh sama noynoy tapi seneng juga karena kita masih bisa main buat terakhir kali sebelum KKN hiks",
+            foto: "assets/timeline-6.jpg"
+        },
+        {
+            tanggal: "Now",
+            judul: "LDR Moment",
+            cerita: "Jujur ini fase terberat dari semua journey kita selama ini, rasanya kangen banget gilakk dan kita juga sering berantem disini. Tapi kita bisa selesain semua masalah yang ada, makasih ya nyonyoy udah bisa sabar ngertiin aku. Semoga kita bisa lewatin masa ini bareng bareng yaa",
+            foto: "assets/timeline-7.PNG"
+        },
+        {
+            tanggal: "???",
+            judul: "???",
+            cerita: "Kira kira next chapter di journey kita ada apa yaa??",
+            foto: "assets/timeline-8.jpeg"
         }
     ],
 
