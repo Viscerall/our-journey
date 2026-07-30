617cc2b654f2318efff3048aa21bbe62b3f957ff
Viscerall <nizaradisaputra12@gmail.com>
2026-07-31 03:32:35 +0700

    last fix

--- config.js
+++ config.js
@@ -92,10 +92,10 @@ const CONFIG = {
     galeri: [
         { url: "assets/galeri-1.jpg", kategori: "1" },
         { url: "assets/galeri-2.jpg", kategori: "2" },
-        { url: "assets/galeri-3.JPG", kategori: "3" },
+        { url: "assets/galeri-3.jpg", kategori: "3" },
         { url: "assets/galeri-4.jpg", kategori: "1" },
         { url: "assets/galeri-5.jpg", kategori: "2" },
-        { url: "assets/galeri-6.JPG", kategori: "3" },
+        { url: "assets/galeri-6.jpg", kategori: "3" },
         { url: "assets/galeri-7.jpg", kategori: "1" },
         { url: "assets/galeri-8.jpg", kategori: "2" },
         { url: "assets/galeri-9.jpg", kategori: "3" },
